47578c14c779407299d545d277384cd1736ca6c3
ichobotov <ivan44@bk.ru>
2025-09-08 16:58:48 +0300

    Stream app

--- StreamCaster_app/simple_server.py
+++ StreamCaster_app/simple_server.py
@@ -4,27 +4,32 @@ import datetime
 import sys
 import time
 
+streampoint = sys.argv[1]
+user = sys.argv[2]
+password = sys.argv[3]
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.connect(('0.0.0.0', 2101))
-streampoint = sys.argv[1]
+server_socket.connect(('10.10.8.49', 2101))
 server_socket.sendall(f'SOURCE server_password /{streampoint}'.encode())
 server_socket.recv(1024)
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(('0.0.0.0', 2101))
-user = sys.argv[2]
-password = sys.argv[3]
-auth_data = base64.b64encode(f'{user}:{password}'.encode())
-client_socket.sendall(f'GET /{streampoint} HTTP/1.1\r\nAuthorization: Basic {auth_data.decode()}\r\n'.encode())
+clients = []
+for i in range(1, 6):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect(('10.10.8.49', 2101))
+    clients.append((f"Client_{i}", client_socket))
+    auth_data = base64.b64encode(f'{user}:{password}'.encode())
+    client_socket.sendall(f'GET /{streampoint} HTTP/1.1\r\nAuthorization: Basic {auth_data.decode()}\r\n'.encode())
 
 try:
     while True:
         data = f"{str(datetime.datetime.now())} example data \r\n".encode()
         server_socket.sendall(data)
-        received_data = client_socket.recv(4096)
-        print(f'Client {user} received "{received_data.decode().rstrip()}" from server for streampoint {streampoint}')
+        for client, socket in clients:
+            received_data = socket.recv(4096)
+            print(f'{client } received "{received_data.decode().rstrip()}" from server for streampoint {streampoint}')
         time.sleep(1)
 except KeyboardInterrupt:
     server_socket.close()
-    client_socket.close()
+    for client, socket in clients:
+        socket.close()
