0bc7862616600d85bd8b7488cd3bdfe43eb6f340
ichobotov <115221037+ichobotov@users.noreply.github.com>
2025-09-08 16:27:36 +0300

    Update simple_example.py

--- StreamCaster_app/simple_example.py
+++ StreamCaster_app/simple_example.py
@@ -4,17 +4,17 @@ import datetime
 import sys
 import time
 
+streampoint = sys.argv[1]
+user = sys.argv[2]
+password = sys.argv[3]
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.connect(('0.0.0.0', 2101))
-streampoint = sys.argv[1]
 server_socket.sendall(f'SOURCE server_password /{streampoint}'.encode())
 server_socket.recv(1024)
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect(('0.0.0.0', 2101))
-user = sys.argv[2]
-password = sys.argv[3]
 auth_data = base64.b64encode(f'{user}:{password}'.encode())
 client_socket.sendall(f'GET /{streampoint} HTTP/1.1\r\nAuthorization: Basic {auth_data.decode()}\r\n'.encode())
 
